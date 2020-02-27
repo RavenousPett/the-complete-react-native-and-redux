02869e097f1e34612657c05e482bf60f4352ec68
Richard <richard@tlgd.co.uk>
2020-02-27 10:31:21 +0000

    Completed first code challenge

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  const  greeting = <Text>Hello to you!</Text>
+  const firstName = 'Richard';
+
   return (
     <View>
-      <Text style={styles.textStyle}>This is the components screen</Text>
-      {greeting}
+      <Text style={styles.textStyle}>Getting started with React Native!</Text>
+
+      <Text style={styles.subHeadingStyle}>My name is {firstName}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30
+  headingStyle: {
+    fontSize: 45
+  },
+  subHeadingStyle: {
+    fontSize: 20
   }
 });
 
