18ed1548759aa905d6fecdc1ff4fae1f8ca6e0c7
Richard <richard@tlgd.co.uk>
2020-04-13 13:41:09 +0100

    Adding button and TouchableOpacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+
+      <Button
+        onPress={() => console.log('Button pressed')}
+        title="Got to Components Demo"
+      />
+
+      <TouchableOpacity onPress={() => console.log('List pressed')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
