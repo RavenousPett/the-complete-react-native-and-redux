1f5cdb815ec793db4719992da96777255d1f64f7
Richard <richard@tlgd.co.uk>
2021-01-07 16:50:04 +0000

    Using flexbox align items

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,19 +3,21 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return <View style={styles.viewStyle}>
-    <Text style={styles.textStyle}>Box Screen</Text>
+    <Text style={styles.textStyle}>Child #1</Text>
+    <Text style={styles.textStyle}>Child #2</Text>
+    <Text style={styles.textStyle}>Child #3</Text>
   </View>
 };
 
 const styles = StyleSheet.create({
   viewStyle: {
+    alignItems: 'flex-end',
     borderWidth: 3,
     borderColor: 'black'
   },
   textStyle: {
-    borderWidth: 10,
-    borderColor: 'red',
-    margin: 20
+    borderWidth: 3,
+    borderColor: 'red'
   }
 });
 
